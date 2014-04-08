886b8e0d15e9b0e8e734036250399d306aaf788f
Chris Roberson <chrisonline@gmail.com>
2014-04-08 10:44:57 -0400

    Top goes to the top of the page now

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,8 +7,13 @@
       evt.preventDefault();
 
       var hash = $(this).attr('href'),
-        element = $(hash);
-      $('html, body').animate({ scrollTop: element.offset().top }, {
+        element = $(hash),
+        top = element.offset().top;
+      if (hash === '#top') {
+        top = 0;
+      }
+
+      $('html, body').animate({ scrollTop: top }, {
         done: function() {
           window.location.hash = hash;
         }
