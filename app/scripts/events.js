d6a050e8ee919491c8c674dfe73b2f56762f3931
Chris Roberson <chrisronline@gmail.com>
2014-07-08 14:32:32 -0400

    Beta

---
+++ app/scripts/events.js
@@ -0,0 +1,70 @@
+(function() {
+  'use strict';
+
+  rivets.bind($('#gigs'), {
+    events: [
+      {
+        date: '06/01/2014',
+        time: '1:30pm',
+        title: 'Car Show @ Webster',
+        link: 'https://goo.gl/maps/otdvB',
+        venue: 'Webster Fireman\'s Field'
+      },
+      {
+        date: '07/04/2014',
+        time: 'All Day',
+        title: 'Private Party'
+      },
+      {
+        date: '07/16/2014',
+        time: '6:30pm',
+        title: 'United Church of Christ',
+        link: 'https://goo.gl/maps/bknbg',
+        venue: 'Klem Rd Webster, NY'
+      },
+      {
+        date: '07/19/2014',
+        time: '7:00pm',
+        title: 'Relay For Life',
+        link: 'https://goo.gl/maps/HZttt',
+        venue: 'Frontier Field'
+      },
+      {
+        date: '08/09/2014',
+        time: 'All Day',
+        title: 'Private Party'
+      },
+      {
+        date: '08/16/2014',
+        time: '4:30pm',
+        title: 'Webster Village Days',
+        link: 'https://goo.gl/maps/7RYQA',
+        venue: 'Webster, NY'
+      },
+      {
+        date: '09/06/2014',
+        time: 'All Day',
+        title: 'Private Party'
+      },
+      {
+        date: '09/27/2014',
+        time: 'All Day',
+        title: 'Private Party'
+      },
+      {
+        date: '10/11/2014',
+        time: '7:00pm',
+        title: 'Legends 102.7FM Fall Dance',
+        link: 'https://goo.gl/maps/l94YE',
+        venue: 'Lodge on the Green @ Greece, NY'
+      },
+      {
+        date: '10/18/2014',
+        time: '1:00pm',
+        title: 'Walworth Harvest Moon Festival',
+        link: 'https://goo.gl/maps/lrpuJ',
+        venue: 'Ginegaw Park'
+      },
+    ]
+  });
+})();
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,37 +1,78 @@
 (function() {
   'use strict';
 
-  var root = $('html, body'),
-    onLoadHash = null;
-
-  function anchorScroll(hash) {
-    var element = $(hash),
-      top = hash === '#top' ? 0 : element.offset().top;
-
-    root.animate({ scrollTop: top }, {
-      done: function() {
-        window.location.hash = hash;
-      }
-    });
-  }
+  var root = $('html, body');
+  var onLoadHash = null;
 
   $(document).ready(function() {
-    $('a[href*=#]').on('click', function(evt) {
+    var headerNav = $('#header nav');
+    var headerNavHeight = headerNav.height();
+    var clonedHeader = headerNav.clone().addClass('floating').addClass('hidden');
+    var toFixedPoint = $('.content').offset().top - headerNavHeight;
+    var sectionIds = $.map($('section[id]'), function(n) {
+      var id = $(n).attr('id');
+      if (id === 'home') id = 'top';
+      return id;
+    });
+    headerNav.before(clonedHeader);
+    headerNav = $('#header nav');
+
+    function anchorScroll(hash) {
+      var element = $(hash),
+        top = hash === '#top' ? 0 : element.offset().top - headerNavHeight;
+
+      root.animate({ scrollTop: top }, {
+        done: function() {
+          window.location.hash = hash;
+        }
+      });
+    }
+
+    function scroll() {
+      var scrollTop = $(document).scrollTop();
+      if (scrollTop >= toFixedPoint) {
+        clonedHeader.addClass('visible');
+      }
+      else {
+        clonedHeader.removeClass('visible');
+      }
+
+      headerNav.find('a').removeClass('active');
+      if (scrollTop + $(this).height() === $('body').outerHeight()) {
+        headerNav.find('a[href="#contact"]').addClass('active');
+      }
+      else {
+        for (var i = sectionIds.length - 1; i >= 0; i--) {
+          if (scrollTop >= $('#' + sectionIds[i]).offset().top - headerNavHeight) {
+            headerNav.find('a[href="#' + sectionIds[i] + '"]').addClass('active');
+            break;
+          }
+        }
+      }
+    }
+
+    $(window).on('scroll', scroll);
+
+    $('header nav a[href*=#]').on('click', function(evt) {
       evt.stopImmediatePropagation();
       evt.preventDefault();
 
       anchorScroll($(this).attr('href'));
     });
 
-    if (onLoadHash !== null) {
+    if (onLoadHash) {
       setTimeout(function() {
         anchorScroll(onLoadHash);
-      }, 100);
+        window.location.hash = '';
+        scroll();
+      });
+    }
+    else {
+      scroll();
     }
   });
 
-  if (window.location.hash) {
+  if (window.location.hash && window.location.hash.length) {
     onLoadHash = window.location.hash;
-    window.location.hash = '';
   }
 })();
